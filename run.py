8c706ed35f73ffd15fcc5b7a0c1d65012bbcaeb6
drmatthewclark <mclark@drmatthewclark.com>
2022-08-01 14:39:20 -0400

    fix options

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from divgen import makexample, sdfile
 from rdkit import Chem
 from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers, StereoEnumerationOptions
 import random
+
+EXPAND_ISOMERS = False
 target = int(1e2)
 count = 0
 mset = set() 
@@ -10,25 +12,27 @@ while True:
     try:
         mol = makexample(random.randrange(10,50))
         sd = sdfile(mol)
-        rdkit_mol  = Chem.MolFromMolBlock(sd, sanitize = True)
-        firstsmiles = Chem.MolToSmiles(rdkit_mol, canonical = True, isomericSmiles = True)
+        molecule  = Chem.MolFromMolBlock(sd, sanitize = True)
+        firstsmiles = Chem.MolToSmiles(molecule, canonical = True, isomericSmiles = True)
         shash = hash(firstsmiles)
 
         if shash in mset:
             continue 
 
-        print(firstsmiles) # no isomer version
-
-        #isomers = EnumerateStereoisomers(rdkit_mol)
-        isomers = rdkit_mol
+        if EXPAND_ISOMERS:
+            print(firstsmiles) 
+            isomers = EnumerateStereoisomers(molecule)
+        else:
+            isomers = [molecule]
+    
         for rdkit_mol in isomers:
             smiles = Chem.MolToSmiles(rdkit_mol, canonical = True, isomericSmiles = True)
             mset.add(hash(smiles))
             print(smiles)
 
             count += 1
-            if count ==  target:
-                    break
+            if count >=  target:
+                exit(0)         
 
     except Exception as e:
         #print(e)
